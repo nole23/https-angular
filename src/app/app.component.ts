979deb298457d37080f865c2d1d2b7032f9512f1
nole23 <nole0223@gmail.com>
2020-02-26 09:47:48 +0100

    add maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,20 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AppComponent implements OnInit {
   
-  constructor() { }
+  name = 'Angular 5';
+  lat:any;
+  lng:any;
+  constructor() {  }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.lat = 52.1961472;
+    this.lng = 20.9813504;
+    // if (navigator) {
+    //   navigator.geolocation.getCurrentPosition(pos => {
+    //     this.lng = +pos.coords.longitude;
+    //     this.lat = +pos.coords.latitude;
+    //   });
+    // }
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
+import { AgmCoreModule } from '@agm/core'; 
 
 @NgModule({
   declarations: [
@@ -14,6 +14,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDpY2K42h_7rMNVCVtz1jubSrdX-kB6-9M',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
